f2b7e19d69d4578583f3782f6a05122c106f2763
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-03-06 18:15:59 +0530

    javascript update

--- js/script.js
+++ js/script.js
@@ -55,3 +55,27 @@ var swiper = new Swiper(".books-slider",{
           },
      },
 });
+
+var swiper = new Swiper(".featured-slider",{
+     spaceBetween:10,
+     loop:true,
+     centeredSlides:true,
+     autoplay:{
+          delay:9500,
+          disableOnInteraction:false,
+     },
+     breakpoints:{
+          0:{
+               slidesPerView:1,
+          },
+          450:{
+               slidesPerView:2,
+          },
+          768:{
+               slidesPerView:3,
+          },
+          1024:{
+               slidesPerView:4,
+          },
+     },
+});
